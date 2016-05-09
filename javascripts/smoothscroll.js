2c280e9171d2f89588e0164c202fab5f5b16d9a4
Tyler Mechura <tjm471@nyu.edu>
2016-05-09 12:07:24 -0400

    .1
    The entire first version of the final site for PolyComputer Consultants
    LLC.

---
+++ javascripts/smoothscroll.js
@@ -0,0 +1,43 @@
+//credit: https://jsfiddle.net/cse_tushar/Dxtyu/141/
+//Author Tushar Gupta
+//Accessed 5/7/2016
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 800, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu-center a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#menu-center ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
